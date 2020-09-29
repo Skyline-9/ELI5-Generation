4f63ac3e0597b4f8f70301246c79901904b158d0
Richard Luo <luo.richard@gmail.com>
2020-09-28 20:34:57 -0400

    🚧 Updated scrape to now be able to scrape full history and save to JSON

--- scrape.py
+++ scrape.py
@@ -1,68 +1,111 @@
 import requests
 import pandas as pd
 import json
-import csv
 import time
-import datetime
 
 
-def get_pushshift_data(data_type, **kwargs):
+def crawl_subreddit(subreddit, max_submissions=2000):
     """
-    Gets data from Reddit using the Pushshift API
+    Crawl submissions from a subreddit.
 
     Parameters
     ----------
-    data_type: string
-        'comment' if you are searching for comments
-        'submission' if you are searching for submissions
-
-    **kwargs: keyword arguments
-        These arguments are interpreted as a payload. Here are the following allowed parameters:
-
-        q: search term
-        duration: timeframe as an Epoch value or integer (i.e. 30d for 30 days)
-        after: return results after this date specified as an Epoch value or integer (i.e. 30d for 30 days)
-        before: return results before this date specified as an Epoch value or integer (i.e. 30d for 30 days)
-        size: number of results to return
-        sort_type: string denoting what to sort by ("score", "num_comments", "created_utc")
-        sort: sort in a specific order ("asc" or "desc")
-        aggs: aggregation summary ["author", "link_id", "created_utc", "subreddit"]
-        subreddit: string containing the specific subreddit to search
-
-        More details on PushShift API's documentation
+    subreddit: string
+        The subreddit to crawl.
+    max_submissions: integer
+        The maximum number of submissions to download.
 
     Returns
     -------
-    data : array_like
-        Data
+    A list of submissions.
     """
-    base_url = f"https://api.pushshift.io/reddit/search/{data_type}/"
-    payload = kwargs
-    request = requests.get(base_url, params=payload)
 
-    # data = json.loads(request.text)
-    return request.json()
+    submissions = []
+    last_page = None
+    while last_page != [] and len(submissions) < max_submissions:
+        last_page = crawl_page(subreddit, last_page)
+        submissions += last_page
+        time.sleep(2)
+
+    return submissions[:max_submissions]
+
+
+def crawl_page(subreddit: str, last_page=None):
+    """Crawl a page of results from a given subreddit.
+
+    Parameters
+    ----------
+    subreddit: string
+        The subreddit to crawl.
+    last_page: string
+        The last downloaded page.
+
+    Returns
+    -------
+    A page or results.
+    """
+
+    url = "https://api.pushshift.io/reddit/search/submission"
+    params = {"subreddit": subreddit, "size": 2000, "sort": "desc", "sort_type": "score", "q": "coronavirus"}
+
+    if last_page is not None:
+        if len(last_page) > 0:
+            # resume from where we left at the last page
+            params["before"] = last_page[-1]["created_utc"]
+        else:
+            # the last page was empty, we are past the last page
+            return []
+
+    results = requests.get(url, params)
+
+    if not results.ok:
+        # Something wrong happened
+        raise Exception("Server returned status code {}".format(results.status_code))
+
+    return results.json()["data"]
+
+
+def pretty_print_json(file_path):
+    """
+    Pretty prints the JSON file using 4 spaces as indent
+
+    Parameters
+    ----------
+    file_path: string
+        The file path to the JSON file (including the file.json)
+
+    Returns
+    -------
+    Nothing
+    """
+
+    with open(file_path) as f:
+        output = json.load(f)
+
+    with open(file_path, "w") as twitter_data_file:
+        json.dump(output, twitter_data_file, indent=4, sort_keys=True)
 
 
 def main():
-    data_type = "submission"  # Comments or Submissions
-    query = "coronavirus"  # Add your query
-    duration = "365d"  # Select the timeframe. Epoch value or Integer + "s,m,h,d"
-    size = 1000  # Maximum 1000 comments
-    sort_type = "score"  # Sort by score (Accepted: "score", "num_comments", "created_utc")
-    sort = "desc"  # Sort descending
-    aggs = "author"  # "author", "link_id", "created_utc", "subreddit"
-    subreddit = "explainlikeimfive"
+    data = crawl_subreddit("explainlikeimfive")
+    df = pd.DataFrame(data=data)
 
-    data = get_pushshift_data(data_type=data_type, q=query, after=duration, size=size, sort_type=sort_type, sort=sort,
-                              subreddit=subreddit)
+    # Display all rows when printing out
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
 
-    # Called selftext
-    data = data.get("data")[1]['selftext']
-    print(data)
+    # Filter only for the columns we care about
+    df = df.filter(['id', 'link_flair_text', 'selftext', 'title', 'url', 'score'])
 
-    # df = pd.DataFrame.from_records(data)[0:10]
-    # print(df)
+    # Filter out all the removed texts
+    df = df[~df['selftext'].str.contains('[deleted]', na=False)]
+
+    # Export to JSON
+    df.to_json('Export_DataFrame.json')
+    pretty_print_json('Export_DataFrame.json')
+
+    print(df)
 
 
 if __name__ == '__main__':
