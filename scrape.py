c8559d97a8271416ffef138d4cb054a87fac803b
Richard Luo <luo.richard@gmail.com>
2020-10-14 01:57:04 -0400

    🎉Added PRAW scraping

--- scrape.py
+++ scrape.py
@@ -1,17 +1,31 @@
 import praw
+import pandas as pd
+import os
+from dotenv import load_dotenv
 
 
 def main():
-    reddit = praw.Reddit(client_id="1tuEGFxlLsiqEA", client_secret="Z0yvyOrukSKcX4hh_SUoep3oD5Q",
-                         password="GTATuesday630",
-                         user_agent="eli5 scrape by u/GT-ELI5",
-                         username="GT-ELI5")
+    # Reading in client_id and client_secret
+    load_dotenv()
+    client_id = os.getenv('client_id')
+    client_secret = os.getenv('client_secret')
 
+    # Authentication
+    reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, password="GTATuesday630",
+                         user_agent="eli5 scrape by u/GT-ELI5", username="GT-ELI5")
+
+    # Start scraping data into a pandas dataframe
     subreddit = reddit.subreddit("explainlikeimfive")
+    df = pd.DataFrame(columns=['Title', 'Score', 'Answer'])
+
+    i = 0
     for submission in subreddit.top(limit=1):
-        print(submission.title)
-        print(submission.score)
-        print(list(submission.comments)[1].body)
+        df.loc[i] = [submission.title, submission.score, list(submission.comments)[1].body]
+        i = i + 1
+
+    # Save as json
+    df.to_json('eli5.json')
+    print(df)
 
 
 if __name__ == '__main__':
