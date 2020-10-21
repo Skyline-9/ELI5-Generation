f639bad6c051bf9342f16d22cd633f9cb88e0fc5
Richard Luo <luo.richard@gmail.com>
2020-10-21 13:16:58 -0400

    Using psaw

--- Data Collection/scrape.py
+++ Data Collection/scrape.py
@@ -2,29 +2,39 @@ import praw
 import pandas as pd
 import os
 from dotenv import load_dotenv
+from psaw import PushshiftAPI
 
 
 def main():
-    # Reading in client_id and client_secret
+    # Reading in client_id, client_secret, username, and password
     load_dotenv()
     client_id = os.getenv('client_id')
     client_secret = os.getenv('client_secret')
+    username = os.getenv('username')
+    password = os.getenv('password')
 
     # Authentication
-    reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, password="GTATuesday630",
-                         user_agent="eli5 scrape by u/GT-ELI5", username="GT-ELI5")
+    reddit = praw.Reddit(client_id=client_id, client_secret=client_secret, password=password,
+                         user_agent="eli5 scrape by u/GT-ELI5", username=username)
 
-    # Start scraping data into a pandas dataframe
-    subreddit = reddit.subreddit("explainlikeimfive")
+    # PSAW wrap
+    api = PushshiftAPI(reddit)
+    gen = api.search_submissions(subreddit='explainlikeimfive', sort_type='score', limit=3)
+    gen = list(gen)
+
     df = pd.DataFrame(columns=['Title', 'Score', 'Answer'])
+    for i, submission in enumerate(gen):
+        comments = list(submission.comments)
 
-    i = 0
-    for submission in subreddit.top(limit=3):
-        df.loc[i] = [submission.title, submission.score, list(submission.comments)[1].body]
-        i = i + 1
+        # Trying to find first non-stickied post (not a mod post)
+        index = 0
+        while comments[index].stickied:
+            index = index + 1
+        
+        # Add to pandas dataframe
+        df.loc[i] = [submission.title, submission.score, comments[index].body]
 
-    # Save as json
-    df.to_json(os.path.join('Data Collection', 'eli5.json'))
+    df.to_json('data.json')
     print(df)
 
 
